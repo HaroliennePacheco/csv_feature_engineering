75aeb0f0efcef5baa31810247a0d483908f7b460
HaroliennePacheco <haroliennep@gmail.com>
2026-02-15 17:23:18 +0800

    Update and rename 4.py to time_based_feature_extraction.py

--- time_based_feature_extraction.py
+++ time_based_feature_extraction.py
@@ -1,15 +1,10 @@
 import pandas as pd
 
 def time_based_feature_extraction(df):
-    # Convert Date column to datetime format
     df['Date'] = pd.to_datetime(df['Date'])
-
-    # Extract Year, Month, and Day
     df['Year'] = df['Date'].dt.year
     df['Month'] = df['Date'].dt.month
     df['Day'] = df['Date'].dt.day
-
-    # Extract Day Name (Monday, Tuesday, etc.)
     df['Day_Name'] = df['Date'].dt.day_name()
 
     return df
